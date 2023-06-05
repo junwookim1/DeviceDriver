b46a323632c5ab6c5463893c90f3f2d45cb48f74
junwookim <junwoo0824@gmail.com>
2023-06-05 15:43:19 +0900

    Add read function and test cases

--- Project4/DeviceDriver.cpp
+++ Project4/DeviceDriver.cpp
@@ -1,16 +1,30 @@
 #include "DeviceDriver.h"
+#include <windows.h>
+#include <vector>
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
-{}
+{
+}
+
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+	vector<int> read_vector;
+	int first = m_hardware->read(address);
+	read_vector.push_back(first);
+	for (int i = 1; i < 5; i++)
+	{
+		Sleep(10);
+		int next = m_hardware->read(address);
+		if (first != next) throw std::exception("Read Fail");
+	}
+
+	return first;
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
-    m_hardware->write(address, (unsigned char)data);
+	// TODO: implement this method
+	m_hardware->write(address, (unsigned char)data);
 }
