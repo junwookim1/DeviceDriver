8b2584cc108a42c765646ef4d4699e71ee40dc75
junwookim <junwoo0824@gmail.com>
2023-06-05 15:53:37 +0900

    Remove unused code

--- Project4/DeviceDriver.cpp
+++ Project4/DeviceDriver.cpp
@@ -1,21 +1,20 @@
 #include "DeviceDriver.h"
+
+#include <exception>
 #include <windows.h>
-#include <vector>
+
 using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {
 }
 
-
 int DeviceDriver::read(long address)
 {
-	vector<int> read_vector;
 	int first = m_hardware->read(address);
-	read_vector.push_back(first);
+
 	for (int i = 1; i < 5; i++)
 	{
-		Sleep(10);
 		int next = m_hardware->read(address);
 		if (first != next) throw std::exception("Read Fail");
 	}
